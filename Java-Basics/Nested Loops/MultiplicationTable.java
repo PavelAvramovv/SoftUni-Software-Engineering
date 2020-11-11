6c858e33fd88c7e85ea2510ddde693bd5c551ca3
Pavel Avramov <65818074+PavelAvramovv@users.noreply.github.com>
2020-11-11 19:40:28 +0200

    Add files via upload

---
+++ Java-Basics/Nested Loops/MultiplicationTable.java
@@ -0,0 +1,12 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+
+        for (int firstNumber = 1; firstNumber <=10; firstNumber++) {
+
+            for (int secondNumber = 1; secondNumber <=10; secondNumber++) {
+                System.out.printf("%d * %d = %d%n",firstNumber,secondNumber,firstNumber*secondNumber);
+            }
+
+        }
+    }
+}
