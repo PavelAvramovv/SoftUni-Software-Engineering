1d305d546e0611a34aff04d8b5e0400c05cdbef9
Pavel Avramov <65818074+PavelAvramovv@users.noreply.github.com>
2021-03-17 01:48:42 +0200

    Add files via upload

---
+++ Methods Homework/Calculations.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Calculations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+      /*  //добавете "," умножете "," извадете "," разделете "
+        int PACO = Integer.parseInt(scanner.nextLine());
+        String asd = */
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
